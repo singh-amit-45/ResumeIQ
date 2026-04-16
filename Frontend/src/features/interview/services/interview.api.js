26b1440cf03db51dcf33b0286143fe034eb0b03d
Amit Kumar Singh <amitsingh100014@gmail.com>
2026-04-16 16:08:48 +0530

    Change API base URL to production endpoint

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://resumeiq-backend-053s.onrender.com",
     withCredentials: true,
 })
 
